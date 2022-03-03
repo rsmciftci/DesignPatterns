eead3ae0b0b527e26cdcf2a8b8236319c880168e
rsmciftci <rsmciftci@gmail.com>
2022-03-03 11:09:28 +0300

    Prototype Pattern

--- src/rsmciftci/Main.java
+++ src/rsmciftci/Main.java
@@ -1,13 +1,26 @@
 package rsmciftci;
 
-import rsmciftci.creationalpatterns.singleton.first.Singleton;
-import rsmciftci.creationalpatterns.singleton.second.SingleObject;
+import rsmciftci.creationalpatterns.prototype.first.PlasticTree;
+import rsmciftci.creationalpatterns.prototype.first.Position;
+import rsmciftci.creationalpatterns.prototype.first.Tree;
 
 public class Main {
     public static void main(String[] args){
 
-        SingleObject instance = SingleObject.getInstance();
-        instance.showMessage();
+        PlasticTree plasticTree = new PlasticTree(50L,1L);
+        Position position = new Position(1, 5);
+        plasticTree.setPosition(position);
+
+
+        PlasticTree copy = (PlasticTree) plasticTree.copy();
+
+        System.out.println(plasticTree);
+        System.out.println(copy);
+
+        // Notice Deep Copy
+        System.out.println(plasticTree == copy);
+        System.out.println(plasticTree.toString().equals(copy.toString()));
+
 
     }
 }
